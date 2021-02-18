23c85b87b94f05ffc53655a6d8362dbb00c9b8a6
Jackeline Evellyn <jackelineeteixeira@gmail.com>
2021-02-18 15:29:07 -0300

    A palavra this e encapsulamento

---
+++ unidade5 - Construtores, this, sobrecarga, encapsulamento/54. Palavra this/54. Palavra this/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+
+namespace _54._Palavra_this
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // A palavra this possui três funções
+
+            //#1 Diferenciar atributos de variaveis locais
+            //Não muito usado em C#, pois os atributos são colocados em letras minusculas
+            //mas bastante usado em Java que não possui esse padrão
+            //exemplo de uso
+
+            /*
+             public Produto (string Nome, double Preco){
+                this.Nome = Nome;
+                this.Preco = Preco;
+                Quantidade = 0;
+            }
+             */
+
+
+            //#2 : Quando vai refereciar outro construtor em um construtor
+            //No caso irá reaproveitar o que é colcado no construtor padrão
+
+            /*
+            public Produto()
+            {
+                Quantidade = 0;
+            } 
+            //ele estará reaproveitando o Quantidade
+            public Produto (string nome, double preco) : this(){ 
+                Nome = nome;
+                Preco = preco;
+            }
+            public Produto (string nome, double preco, int quantidade) : this(nome, preco){
+                Quantidade = quantidade;
+            }
+            */
+
+            //#3 Passar o próprio objeto como argumento na chamade de um método ou construtor
+            /*
+             class ChessMatch{
+            
+                PlaceNewPiece('e',1, new King(board, Color.White, this)); 
+            //NEsse caso estará passando o proprio objeto
+            }
+             
+             
+             */
+            
+        }
+    }
+}
---
+++ unidade5 - Construtores, this, sobrecarga, encapsulamento/55.Encapsulamento/55.Encapsulamento/Produto.cs
@@ -0,0 +1,42 @@
+﻿using System.Globalization;
+
+namespace _55.Encapsulamento
+{
+    class Produto
+    {
+        //Todos atributos devem ser private
+        private string _nome; // quando é private possui uma outra padronização de nome
+        private double _preco; // underline e letra minuscula
+        private int _quantidade;
+
+        public Produto(string nome, double preco, int quantidade)
+        {
+            _nome = nome;
+            _preco = preco;
+            _quantidade = quantidade;
+        }
+
+        public double ValorTotalEmEstoque()
+        {
+            return _preco * _quantidade;
+        }
+        public void AdicionarProdutos(int quantidade)
+        {
+            _quantidade += quantidade;
+        }
+        public void RemoverProdutos(int quantidade)
+        {
+            _quantidade -= quantidade;
+        }
+        public override string ToString()
+        {
+            return _nome
+            + ", $ "
+            + _preco.ToString("F2", CultureInfo.InvariantCulture)
+            + ", "
+            + _quantidade
+            + " unidades, Total: $ "
+            + ValorTotalEmEstoque().ToString("F2", CultureInfo.InvariantCulture);
+        }
+    }
+}
