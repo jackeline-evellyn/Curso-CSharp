0d4c04b151b1f55738b576865d5973b2703efb90
Jackeline Evellyn <jackelineeteixeira@gmail.com>
2021-02-19 18:12:30 -0300

    Finalização Unidade 5

--- unidade5 - Construtores, this, sobrecarga, encapsulamento/60. Exercicio de fixacao/60. Exercicio de fixacao/ContaBancaria.cs
+++ unidade5 - Construtores, this, sobrecarga, encapsulamento/60. Exercicio de fixacao/60. Exercicio de fixacao/ContaBancaria.cs
@@ -1,10 +1,44 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Globalization;
 
 namespace _60._Exercicio_de_fixacao
 {
     class ContaBancaria
     {
+        public int NumeroConta { get; private set; }
+        public string NomeTitular { get;  set; } // é possível alterar o nome
+        public double Saldo { get; private set; } // só pode ser alterado por deposito ou saque
+
+        public ContaBancaria(int numeroConta, string nomeTitular)
+        {
+            NumeroConta = numeroConta;
+            NomeTitular = nomeTitular;
+            Saldo = 0.0;
+        }
+        public ContaBancaria(int numeroConta, string nomeTitular, double depositoInicial) : this(numeroConta, nomeTitular){
+            Deposito(depositoInicial); 
+        }
+        public void Deposito (double valor)
+        {
+            Saldo += valor;
+        }
+        public void Saque (double valor)
+        {
+            Saldo -= valor + 5.00;
+        }
+        public override string ToString()
+        {
+            return "Conta "
+                + NumeroConta
+                + " , Titular: "
+                + NomeTitular
+                + " , Saldo: $ "
+                + Saldo.ToString("F2", CultureInfo.InvariantCulture);
+        }
+
+
+
     }
 }
--- unidade5 - Construtores, this, sobrecarga, encapsulamento/60. Exercicio de fixacao/60. Exercicio de fixacao/Program.cs
+++ unidade5 - Construtores, this, sobrecarga, encapsulamento/60. Exercicio de fixacao/60. Exercicio de fixacao/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Globalization;
 
 namespace _60._Exercicio_de_fixacao
 {
@@ -6,7 +7,42 @@ namespace _60._Exercicio_de_fixacao
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            ContaBancaria contaBancaria;
+
+            Console.Write("Entre com o número da conta: ");
+            int numeroConta = int.Parse(Console.ReadLine());
+            Console.Write("Entre com o titular da conta: ");
+            string nomeTitular = Console.ReadLine();
+            Console.Write("Haverá deposito inicial? ");
+            char resposta = char.Parse(Console.ReadLine());
+
+            if(resposta == 'S' || resposta == 's')
+            {
+                Console.Write("Entre o valor do deposito inicial: ");
+                double depositoInicial = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+                contaBancaria = new ContaBancaria(numeroConta, nomeTitular, depositoInicial);
+            }
+            else
+            {
+                contaBancaria = new ContaBancaria(numeroConta, nomeTitular);
+            }
+
+            Console.WriteLine();
+            Console.WriteLine("Dados da conta: ");
+            Console.WriteLine(contaBancaria);
+
+            Console.WriteLine();
+            Console.Write("Entre um valor para deposito: ");
+            double valor = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            contaBancaria.Deposito(valor);
+            Console.WriteLine(contaBancaria);
+            
+            Console.WriteLine();
+            Console.Write("Entre um valor para saque: ");
+            valor = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            contaBancaria.Saque(valor);
+            Console.WriteLine(contaBancaria);
+        
         }
     }
 }
