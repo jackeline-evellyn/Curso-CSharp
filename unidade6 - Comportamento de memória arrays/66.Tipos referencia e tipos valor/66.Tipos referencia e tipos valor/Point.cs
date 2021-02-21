f91467084432543d33a72c3b18d7009807bea9e9
Jackeline Evellyn <jackelineeteixeira@gmail.com>
2021-02-21 18:32:16 -0300

    66

---
+++ unidade6 - Comportamento de memória arrays/66.Tipos referencia e tipos valor/66.Tipos referencia e tipos valor/Point.cs
@@ -0,0 +1,12 @@
+﻿namespace _66.Tipos_referencia_e_tipos_valor
+{
+    struct Point
+    {
+        public double X, Y;
+
+        public override string ToString()
+        {
+            return "(" + X + " ," + Y + ")";
+        }
+    }
+}
---
+++ unidade6 - Comportamento de memória arrays/66.Tipos referencia e tipos valor/66.Tipos referencia e tipos valor/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace _66.Tipos_referencia_e_tipos_valor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Point p; //No tipo struct nao é necessario colocar
+            // " = new Point ();
+            p.X = 10; // é necessário inicializar
+            p.Y = 20;
+            Console.WriteLine(p);
+
+            p = new Point(); // No entanto ele pode receber o valor de new
+            Console.WriteLine(p);
+        }
+    }
+}
