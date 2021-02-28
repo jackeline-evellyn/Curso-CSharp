45a6f91307104f4d31f4cc6b46a7f45e672a5706
Jackeline Evellyn <jackelineeteixeira@gmail.com>
2021-02-28 18:52:58 -0300

    88 e 90 - DateTime e TimeSpan

--- unidade7 - Tópicos Especiais/88.DateTime -data e hora/88.DateTime -data e hora/Program.cs
+++ unidade7 - Tópicos Especiais/88.DateTime -data e hora/88.DateTime -data e hora/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Globalization;
 
 namespace _88.DateTime__data_e_hora
 {
@@ -9,8 +10,46 @@ namespace _88.DateTime__data_e_hora
             //Representa um instante
             //Tipo struct - tipo valor
 
+            //Now - Horário atual
+            DateTime d1 = DateTime.Now;
+            Console.WriteLine(d1);
+
+            //Internamente é colocado o numero e Ticks (100 nanosegundos)
+            Console.WriteLine(d1.Ticks);
+
+            DateTime d2 = new DateTime(2018, 11, 25);
+            DateTime d3 = new DateTime(2018, 11, 25, 20, 45, 3);
+            DateTime d4 = new DateTime(2018, 11, 25, 20, 45, 3, 500);
+
+            //Today- Dia atual com hora zerada
+            DateTime d5 = DateTime.Today;
+
+            //UtfNow - Horario no fuso horario de greenwich
+            DateTime d6 = DateTime.UtcNow;
+
+            //Parse - Recebe em formato de string
+            DateTime d7 = DateTime.Parse("2000-08-15");
+            DateTime d8 = DateTime.Parse("2000-08-15 13:05:58");
+            DateTime d9 = DateTime.Parse("15/08/2000");
+            DateTime d10 = DateTime.Parse("15/08/2000 13:05:58");
+
+            //ParseExact - Quando quer determinar o formato da data
+            DateTime d11 = DateTime.ParseExact("2000-08-15", "yyyy-MM-dd", CultureInfo.InvariantCulture );
+            DateTime d12 = DateTime.ParseExact("15/08/2000 13:05:58", "dd/MM/yyyy HH:mm:ss", CultureInfo.InvariantCulture);
 
 
+
+            Console.WriteLine(d2);
+            Console.WriteLine(d3);
+            Console.WriteLine(d4);
+            Console.WriteLine(d5);
+            Console.WriteLine(d6);
+            Console.WriteLine(d7);
+            Console.WriteLine(d8);
+            Console.WriteLine(d9);
+            Console.WriteLine(d10);
+            Console.WriteLine(d11);
+            Console.WriteLine(d12);
         }
     }
 }
---
+++ unidade7 - Tópicos Especiais/90.TimeSpan -duração/90.TimeSpan -duração/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+
+namespace _81.TimeSpan__duracao
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //TimeSpan - duração
+            //Também em forma de Ticks
+
+            //TimeSpan passando hora minuto e segundo
+            TimeSpan t1 = new TimeSpan(0, 1, 30); //hora minuto segundo
+
+            //TimeSpan vazio- 0:00:00
+            TimeSpan t2 = new TimeSpan();
+
+            //TimeSpan passando a quantidade de Ticks
+            TimeSpan t3 = new TimeSpan(9000000000L); // letra L para identificar que é um long
+
+            //TimeSpan com dia, hora, minutos e segundos
+            TimeSpan t4 = new TimeSpan(1, 2, 11, 21);
+
+            //TimeSpan com dia, hora, minutos, segundos e milisegundo
+            TimeSpan t5 = new TimeSpan(1, 2, 11, 21, 321);
+
+            //TimeSpan equivalente a dias, horas
+            TimeSpan t6 = TimeSpan.FromDays(1.5);
+            TimeSpan t7 = TimeSpan.FromHours(1.5);
+            TimeSpan t8 = TimeSpan.FromMinutes(1.5);
+            TimeSpan t9 = TimeSpan.FromSeconds(1.5);
+            TimeSpan t10 = TimeSpan.FromMilliseconds(1.5);
+            TimeSpan t11 = TimeSpan.FromTicks(900000000L);
+
+            Console.WriteLine(t1);
+            Console.WriteLine(t1.Ticks);
+            Console.WriteLine(t2);
+            Console.WriteLine(t3);
+            Console.WriteLine(t4);
+            Console.WriteLine(t5);
+            Console.WriteLine(t6);
+            Console.WriteLine(t7);
+            Console.WriteLine(t8);
+            Console.WriteLine(t9);
+            Console.WriteLine(t10);
+            Console.WriteLine(t11);
+            {
+
+            }
+        }
+    }
+}
