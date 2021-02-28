9b257e76a7146b47959ae3da98d8125688f2e275
Jackeline Evellyn <jackelineeteixeira@gmail.com>
2021-02-28 16:36:33 -0300

    Topicos especiais

--- unidade6 - Comportamento de memória arrays/81. Exercicio fixacao - matrizes/81. Exercicio fixacao - matrizes/Program.cs
+++ unidade6 - Comportamento de memória arrays/81. Exercicio fixacao - matrizes/81. Exercicio fixacao - matrizes/Program.cs
@@ -13,7 +13,46 @@ namespace _81._Exercicio_fixacao___matrizes
             Console.Write("Digite o numero N: ");
             int n = int.Parse(Console.ReadLine());
 
-            int mat[,] = new
+            int[,] mat = new int[m, n];
+
+            for (int i=0; i < m; i++)
+            {
+                string[] values = Console.ReadLine().Split(' ');
+                for (int j=0; j < n; j++)
+                {
+                    mat[i, j] = int.Parse(values[j]);
+                }
+            }
+
+            int x = int.Parse(Console.ReadLine());
+          
+            for (int i = 0; i < m; i++)
+            {
+                for (int j = 0; j < n; j++)
+                {
+                    if(mat[i,j] == x)
+                    {
+                        Console.WriteLine("Position: " + i + "," + j);
+                        if(j > 0)
+                        {
+                            Console.WriteLine("Left: " + mat[i,j-1]);
+                        }
+                        if(i > 0)
+                        {
+                            Console.WriteLine("Up: "+ mat[i-1,j]);
+                        }
+                        if (j < n - 1)
+                        {
+                            Console.WriteLine("Right: "+mat[i,j+1]);
+                        }
+                        if (i < m - 1)
+                        {
+                            Console.WriteLine("Down: " +mat[i+1,j]);
+                        }
+
+                    }
+                }
+            }
 
         }
     }
---
+++ unidade7 - Tópicos Especiais/85.Inferencia de tipo-var/85.Inferencia de tipo-var/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace _85.Inferencia_de_tipo_var
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Inferir variavel - deduzir
+            //ou seja, coloca "var" e será deduzido o tipo da variavel
+            var x = 10;
+            var y = 20.0;
+            var z = "Maria";
+
+            Console.WriteLine(x);
+            Console.WriteLine(y);
+            Console.WriteLine(z);
+
+        }
+    }
+}
