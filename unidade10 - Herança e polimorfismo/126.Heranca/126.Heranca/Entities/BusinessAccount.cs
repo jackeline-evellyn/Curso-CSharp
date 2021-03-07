f12360660926a4d24ee9cbcc4c13d2d82638c6fd
Jackeline Evellyn <jackelineeteixeira@gmail.com>
2021-03-07 10:10:58 -0300

    herança

---
+++ unidade10 - Herança e polimorfismo/126.Heranca/126.Heranca/Entities/BusinessAccount.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _126.Heranca.Entities
+{
+    //Herança ": Account"
+    class BusinessAccount: Account
+    {
+        public double LoanLimit { get; set; }
+   
+        public BusinessAccount()
+        {
+
+        }
+
+        //: base() -> realiza as atribuições na classe principal
+        public BusinessAccount(int number, string holder, double balance, double loanLimit) : base(number, holder, balance)
+        {
+           
+            LoanLimit = loanLimit;
+        }
+
+        public void Loan(double amount)
+        {
+           if (amount <= LoanLimit)
+            {
+                Balance += amount;
+            }
+        }
+
+
+    }
+}
--- unidade10 - Herança e polimorfismo/126.Heranca/126.Heranca/Program.cs
+++ unidade10 - Herança e polimorfismo/126.Heranca/126.Heranca/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using _126.Heranca.Entities;
 
 namespace _126.Heranca
 {
@@ -6,7 +7,21 @@ namespace _126.Heranca
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            /*Herança relação "é-um"
+            Generalização (generica)/ especialização (possui outros atributos)
+            Superclasse (classe base) / subclasse (classe derivada)
+            Herança é uma extensão
+            Herança é uma associação entre classes (e não entre objetos)
+             */
+
+            BusinessAccount account = new BusinessAccount(8010,"Bob Brown", 100.0, 500.0);
+
+            Console.WriteLine(account.Balance);
+
+            //account.Balance(200);
+            //nao consegue, pois é protected
+
+
         }
     }
 }
