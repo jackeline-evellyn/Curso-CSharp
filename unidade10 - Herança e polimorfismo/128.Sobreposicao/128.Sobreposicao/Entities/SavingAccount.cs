efbaa21356c0ae6158ce11db6361e4dc1b0cca19
Jackeline Evellyn <jackelineeteixeira@gmail.com>
2021-03-08 15:20:46 -0300

    128. Sobreposição, palavras virtual, override e base

---
+++ unidade10 - Herança e polimorfismo/128.Sobreposicao/128.Sobreposicao/Entities/SavingAccount.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _128.Sobreposicao.Entities
+{
+    class SavingAccount : Account
+    {
+        public double InterestRate { get; set; }
+
+        public SavingAccount()
+        {
+
+        }
+        public SavingAccount(int number, string holder, double balance, double interestRate)
+              : base(number, holder, balance)
+        {
+            InterestRate = interestRate;
+        }
+
+        public void UpdateBalance()
+        {
+            Balance += Balance * InterestRate;
+        }
+        //overide para sobreescrever o métoo
+        public override void Withdraw(double amount)
+        {
+            base.Withdraw(amount); // pega os valores que já estavam na superclasse
+            Balance -= 2.0;
+        }
+    }
+}
--- unidade10 - Herança e polimorfismo/128.Sobreposicao/128.Sobreposicao/Program.cs
+++ unidade10 - Herança e polimorfismo/128.Sobreposicao/128.Sobreposicao/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using _128.Sobreposicao.Entities;
 
 namespace _128.Sobreposicao
 {
@@ -6,7 +7,18 @@ namespace _128.Sobreposicao
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            //Para que um objeto possa ser sobreposto deve ser incluído nesse o prefixo "virtual"
+            //Ao sobreescrever um metodo devemos usar o prefixo override
+
+            Account acc1 = new Account(1001, "Alex", 500.0);
+            Account acc2 = new SavingAccount(1002, "Anna", 500.0, 0.01);
+
+            acc1.Withdraw(10.0);
+            acc2.Withdraw(10.0);
+
+            Console.WriteLine(acc1.Balance);
+            Console.WriteLine(acc2.Balance);
+
         }
     }
 }
