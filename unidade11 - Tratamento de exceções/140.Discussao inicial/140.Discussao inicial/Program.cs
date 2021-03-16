0f83fd0fe08215c2931a7e241624569f9401659f
Jackeline Evellyn <jackelineeteixeira@gmail.com>
2021-03-16 19:24:23 -0300

    capitulo 11

---
+++ unidade11 - Tratamento de exceções/140.Discussao inicial/140.Discussao inicial/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace _140.Discussao_inicial
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /* Exceção é um erro ou comportamento inesperado do programa em execução
+             * 
+             * 
+             * 
+             */
+
+
+        }
+    }
+}
