3e5702c34b27baac6bdb891d13ea37265d15e3bc
Jackeline Evellyn <jackelineeteixeira@gmail.com>
2021-03-16 21:33:00 -0300

    144

---
+++ unidade11 - Tratamento de exceções/144. Solucao personalizada 2/144. Solucao personalizada 2/Entities/Reservation.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _144._Solucao_personalizada_2.Entities
+{
+    class Reservation
+    {
+        public int RoomNumber { get; set; }
+        public DateTime CheckIn { get; set; }
+        public DateTime CheckOut { get; set; }
+
+        public Reservation()
+        {
+
+        }
+
+        public Reservation(int roomNumber, DateTime checkin, DateTime checkout)
+        {
+            RoomNumber = roomNumber;
+            CheckIn = checkin;
+            CheckOut = checkout;
+        }
+        public int Duration()
+        {
+            TimeSpan duration = CheckOut.Subtract(CheckIn);
+            return (int)duration.TotalDays;
+        }
+        public string UpdateDates(DateTime checkIn, DateTime checkOut)
+        {
+            DateTime now = DateTime.Now;
+            if (checkIn < now || checkOut < now)
+            {
+                return "Error in reservation: Reservation dates for updates must be future dates";
+
+            }
+            
+            if (checkOut <= checkIn)
+            {
+                return "Error in reservation: Check-out date must be after check-in";
+            }
+            else
+            {
+                CheckIn = checkIn;
+                CheckOut = checkOut;
+                return null;
+            }
+        }
+        public override string ToString()
+        {
+            return " Room "
+                + RoomNumber
+                + " , check-in: "
+                + CheckIn.ToString("dd/MM/yyyy")
+                + " , check-out: "
+                + CheckOut.ToString("dd/MM/yyyy")
+                + ", "
+                + Duration()
+                + " nights";
+        }
+    }
+}
+
---
+++ unidade11 - Tratamento de exceções/144. Solucao personalizada 2/144. Solucao personalizada 2/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using _144._Solucao_personalizada_2.Entities;
+
+namespace _144._Solucao_personalizada_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Room number: ");
+            int room = int.Parse(Console.ReadLine());
+            Console.Write("Check-in date (dd/MM/yyyy): ");
+            DateTime checkin = DateTime.Parse(Console.ReadLine());
+            Console.Write("Check-out date (dd/MM/yyyy): ");
+            DateTime checkout = DateTime.Parse(Console.ReadLine());
+
+            //Solucao "menos ruim", sem usar exceção
+            if (checkout <= checkin)
+            {
+                Console.WriteLine("Error in reservation: Check-out date must be after check-in");
+            }
+            else
+            {
+                Reservation reservation = new Reservation(room, checkin, checkout);
+                Console.WriteLine("Reservation: " + reservation);
+
+                Console.WriteLine();
+                Console.WriteLine("Enter data to update the reservation: ");
+
+                Console.Write("Check-in date (dd/MM/yyyy): ");
+                checkin = DateTime.Parse(Console.ReadLine());
+                Console.Write("Check-out date (dd/MM/yyyy): ");
+                checkout = DateTime.Parse(Console.ReadLine());
+
+                string error = reservation.UpdateDates(checkin, checkout);
+
+                if(error != null)
+                {
+                    Console.WriteLine("Error in reservation: "+ error);
+                }
+                else
+                {
+                    Console.WriteLine("Reservation: " + reservation);
+                }
+            }
+
+
+        }
+    }
+}
