c2a4cbc8c68ea9df86c10ec8591168499f7fe07d
Jackeline Evellyn <jackelineeteixeira@gmail.com>
2021-03-16 22:10:13 -0300

    145

---
+++ unidade11 - Tratamento de exceções/145. Excecoes personalizadas 3/145. Excecoes personalizadas 3/Entities/Exceptions/DomainException.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _145._Excecoes_personalizadas_3.Entities.Exceptions
+{
+    class DomainException : ApplicationException
+    {
+        public DomainException(string message) : base(message)
+        {
+
+        }
+
+    }
+}
--- unidade11 - Tratamento de exceções/145. Excecoes personalizadas 3/145. Excecoes personalizadas 3/Program.cs
+++ unidade11 - Tratamento de exceções/145. Excecoes personalizadas 3/145. Excecoes personalizadas 3/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using _145._Excecoes_personalizadas_3.Entities;
+using _145._Excecoes_personalizadas_3.Entities.Exceptions;
 
 namespace _145._Excecoes_personalizadas_3
 {
@@ -6,7 +8,46 @@ namespace _145._Excecoes_personalizadas_3
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            try
+            {
+                Console.Write("Room number: ");
+                int room = int.Parse(Console.ReadLine());
+                Console.Write("Check-in date (dd/MM/yyyy): ");
+                DateTime checkin = DateTime.Parse(Console.ReadLine());
+                Console.Write("Check-out date (dd/MM/yyyy): ");
+                DateTime checkout = DateTime.Parse(Console.ReadLine());
+
+                //Solucao "menos ruim", sem usar exceção
+
+                Reservation reservation = new Reservation(room, checkin, checkout);
+                Console.WriteLine("Reservation: " + reservation);
+
+                Console.WriteLine();
+                Console.WriteLine("Enter data to update the reservation: ");
+                Console.Write("Check-in date (dd/MM/yyyy): ");
+                checkin = DateTime.Parse(Console.ReadLine());
+                Console.Write("Check-out date (dd/MM/yyyy): ");
+                checkout = DateTime.Parse(Console.ReadLine());
+
+
+                reservation.UpdateDates(checkin, checkout);
+                Console.WriteLine("Reservation: " + reservation);
+            }
+            catch (DomainException e ) {
+                Console.WriteLine("Error in reservation: "+ e.Message);
+            }
+            catch (FormatException e)
+            {
+                Console.WriteLine("Format error: "+ e.Message);
+            }
+            catch(Exception e)
+            {
+                Console.WriteLine("Unexpected error: " + e.Message);
+            }
+            }
+            
+
         }
     }
-}
+
+    
