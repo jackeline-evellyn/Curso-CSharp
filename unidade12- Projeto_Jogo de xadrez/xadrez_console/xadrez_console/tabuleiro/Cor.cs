5d1f2c49aaf52c8fde3fb1efe1f3e611f7385e2f
Jackeline Evellyn <jackelineeteixeira@gmail.com>
2021-03-17 22:01:37 -0300

    comecando a implementar o tabuleiro

---
+++ unidade12- Projeto_Jogo de xadrez/xadrez_console/xadrez_console/tabuleiro/Cor.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace xadrez_console.tabuleiro
+{
+    enum Cor
+    {
+        Branca,
+        Preta,
+        Amarelo,
+        Azul,
+        Vermelho, 
+        Laranja
+    }
+}
