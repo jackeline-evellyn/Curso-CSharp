6b1037b6a294306116ff7b852af3f3a46b59dff2
Jackeline Evellyn <jackelineeteixeira@gmail.com>
2021-03-17 21:49:31 -0300

    Criando a classe Posicao

---
+++ unidade12- Projeto_Jogo de xadrez/xadrez_console/xadrez_console/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using xadrez_console.tabuleiro;
+
+namespace xadrez_console
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Posicao p;
+            p = new Posicao(3, 4);
+            Console.WriteLine("Posicao: " + p);
+            
+
+        }
+    }
+}
