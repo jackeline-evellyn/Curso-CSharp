d98fb76e6b363249f019c10483d7576a2d0ed281
Jackeline Evellyn <jackelineeteixeira@gmail.com>
2021-03-24 17:32:40 -0300

    187. File, FileInfo e IOException

---
+++ unidade13-Trabalhando com arquivos/186. File FileInfo IOException/186. File FileInfo IOException/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.IO;
+
+namespace _186._File_FileInfo_IOException
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string sourcePath = @"C:\Windows.old\WINDOWS\Temp\file1.txt";
+            string targetPath = @"C:\Windows.old\WINDOWS\Temp\file2.txt";
+
+            try
+            {
+                FileInfo fileInfo = new FileInfo(sourcePath);
+                fileInfo.CopyTo(targetPath);
+                string[] lines = File.ReadAllLines(sourcePath);
+                foreach (string line in lines)
+                {
+                    Console.WriteLine(line);
+                }
+            }
+            catch(IOException e)
+            {
+                Console.WriteLine("An error occured");
+                Console.WriteLine(e.Message);
+            }
+        }
+    }
+}
