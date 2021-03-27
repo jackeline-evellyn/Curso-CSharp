f16db65ca9db843da53d5453cbcba24365524ff4
Jackeline Evellyn <jackelineeteixeira@gmail.com>
2021-03-27 18:17:04 -0300

    187

---
+++ unidade13-Trabalhando com arquivos/187.FileStream StremReader/187.FileStream StremReader/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _187.FileStream_StremReader
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
