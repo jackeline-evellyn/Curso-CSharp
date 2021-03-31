71eb5d56456cac254e2c7cad0cd2d2c3476ca6c5
Jackeline Evellyn <jackelineeteixeira@gmail.com>
2021-03-31 16:52:25 -0300

    191

--- unidade13-Trabalhando com arquivos/190.Directory DirectoryInfo/190.Directory DirectoryInfo/Program.cs
+++ unidade13-Trabalhando com arquivos/190.Directory DirectoryInfo/190.Directory DirectoryInfo/Program.cs
@@ -8,10 +8,32 @@ namespace _190.Directory_DirectoryInfo
     {
         static void Main(string[] args)
         {
+            //Directory, DirectoryInfo
+            //Operações com pastas (create, enumerate, get files, etc)
+
             string path = @"c:\temp\myfolder";
 
             try
             {
+                //1- Listar todas pastas
+                IEnumerable<string> folders = Directory.EnumerateDirectories(path, "*.*", SearchOption.AllDirectories);
+                Console.WriteLine("FOLDERS:");
+                foreach(string s in folders)
+                {
+                    Console.WriteLine(s);
+                }
+                Console.WriteLine();
+
+                //2- Listar todos arquivos a partir de uma pasta
+                var files = Directory.EnumerateFiles(path, "*.*", SearchOption.AllDirectories);
+                Console.WriteLine("FILES:");
+                foreach (string s in files)
+                {
+                    Console.WriteLine(s);
+                }
+
+                //3- Criar uma pasta
+                Directory.CreateDirectory(path + @"\newfolder");
 
             }
             catch(IOException e)
