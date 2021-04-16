aa23309b9a5890ea1efb99402ac608f43468a419
Jackeline Evellyn <jackelineeteixeira@gmail.com>
2021-04-16 12:16:03 -0300

    fim capitulo Generic, Set e Dictionary

---
+++ unidade15-Generics, Set, Dictionary/213.Dictionary_SortedDictionary/213.Dictionary_SortedDictionary/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _213.Dictionary_SortedDictionary
+{
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /* Em outras linguagens está como map
+            Dictionary<TKey, TValue>
+            - Não admite repetições na chave
+            - Os elementos são indexados pelo objeto chave (não possuem posição)
+            - Acesso, inserção e remoão são rápidos
+
+             Dictionary
+                - Armazenamento em tabela hash
+                - Extremamente rápido: inserção, remoção e busca
+                - A ordem dos elementos não é garatida
+
+            SortedDictionary
+                - Armazenamento em árvore
+                - Rápido: inserção, remoção e busca
+                - Os elementos são arazenados ordenadamente conforme implementação IComparer<T>
+             */
+
+            Dictionary<string, string> cookies = new Dictionary<string, string>();
+
+            cookies["user"] = "maria";
+            cookies["email"] = "maria@gmail.com";
+            cookies["phone"] = "99712234";
+            cookies["phone"] = "83737388"; // apenas subscreverá o valor
+
+            Console.WriteLine(cookies["phone"]);
+            Console.WriteLine(cookies["email"]);
+
+            cookies.Remove("email");
+
+            if (cookies.ContainsKey("email"))
+            {
+                Console.WriteLine(cookies["email"]);
+            }
+            else
+            {
+                Console.WriteLine("There is no 'email' key");
+            }
+            Console.WriteLine();
+
+            Console.WriteLine("Size: " + cookies.Count);
+
+            Console.WriteLine("ALL COOKIES: ");
+            foreach (KeyValuePair<string, string> item in cookies)
+          //foreach (var item in cookies)
+            {
+                    Console.WriteLine(item.Key + ": " + item.Value);
+            }
+
+        }
+    }
+}
---
+++ unidade15-Generics, Set, Dictionary/214.Exercicio/214.Exercicio/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Globalization;
+using System.Collections.Generic;
+using System.IO;
+
+namespace _214.Exercicio
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.Write("Enter file full path: ");
+            string path = Console.ReadLine();
+
+            try
+            {
+                using(StreamReader sr = File.OpenText(path))
+                {
+                    Dictionary<string, int> election = new Dictionary<string, int>();
+                    while (!sr.EndOfStream)
+                    {
+                        string[] votingRecord = sr.ReadLine().Split(',');
+                        string name = votingRecord[0];
+                        int votes = int.Parse(votingRecord[1]);
+
+                        if (election.ContainsKey(name))
+                        {
+                            election[name] += votes;
+                        }
+                        else
+                        {
+                            election[name] = votes;
+                        }
+                    }
+                    foreach (KeyValuePair<string, int> item in election)
+                    {
+                        Console.WriteLine(item.Key + ": " + item.Value);
+                    }
+                }
+            }
+            catch (IOException e)
+            {
+                Console.WriteLine("An error occured");
+                Console.WriteLine(e.Message);
+            }
+        }
+    }
+}
