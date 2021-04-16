4271b642a7d4080dfa06917c754bedb45afee97f
Jackeline Evellyn <jackelineeteixeira@gmail.com>
2021-04-16 15:06:14 -0300

    Tópicos especiais

---
+++ unidade16 - Tópicos especiais parte 2/217.ExtensionMethods/217.ExtensionMethods/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace _217.ExtensionMethods
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DateTime dt = new DateTime(2021,04, 16, 8, 10, 45);
+
+            Console.WriteLine(dt.ElapsedTime());
+        }
+    }
+}
---
+++ unidade16 - Tópicos especiais parte 2/217.ExtensionMethods2/217.ExtensionMethods2/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace _217.ExtensionMethods2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string s1 = "Good morning dear students!";
+            Console.WriteLine(s1.Cut(10));
+       
+        
+        }
+    }
+}
