ed922266107d41ccdf186633874e97c61a6bb97f
Jackeline Evellyn <jackelineeteixeira@gmail.com>
2021-04-21 14:15:39 -0300

    Delegates

---
+++ unidade17-Expressões lambda, delegate, LINQ/220.ProgracaoFuncional/220.ProgracaoFuncional/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace _220.ProgracaoFuncional
+{
+    class Program
+    {
+        public static int globalValue = 10;
+        static void Main(string[] args)
+        {
+            int[] vect = new int[] { 2, 4, 5 };
+            ChangeOddValues(vect);
+            Console.WriteLine(string.Join(" ", vect));
+        }
+        public static void ChangeOddValues(int[] numbers)
+        {
+            for(int i = 0; i < numbers.Length; i++)
+            {
+                if(numbers [i] % 2 != 0)
+                {
+                    numbers[i] += globalValue;
+                }
+            }
+        }
+    }
+}
---
+++ unidade17-Expressões lambda, delegate, LINQ/221.Delegates/221.Delegates/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using _221.Delegates.Services;
+
+namespace _221.Delegates
+{
+    class Program
+    {
+        delegate double BinaryNumericOperation(double n1, double n2);// operação numerica binaria 
+
+        static void Main(string[] args)
+        {
+            /* É tipo referencia
+             * Delegate pré definidos: action, func e predicate
+             */
+
+            double a = 10;
+            double b = 12;
+
+            BinaryNumericOperation op = CalculatorService.Max;
+            //Possui type safety, se tentar passar um valor que nao retorne double
+            //ou que nao seja dois valores ele dará erro no compilador e não na execução
+
+            double result = op.Invoke(a, b); // invoke é opcional
+            Console.WriteLine(result);
+
+
+        }
+    }
+}
