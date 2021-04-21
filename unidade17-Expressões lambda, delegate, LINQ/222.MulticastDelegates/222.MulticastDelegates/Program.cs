4d198ddbd48d4424be5100669953fc14c1737259
Jackeline Evellyn <jackelineeteixeira@gmail.com>
2021-04-21 15:15:04 -0300

    predicate com funcao lambda

---
+++ unidade17-Expressões lambda, delegate, LINQ/222.MulticastDelegates/222.MulticastDelegates/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using _222.MulticastDelegates.Services;
+
+namespace _222.MulticastDelegates
+{
+    class Program
+    {
+        delegate void BinaryNumericOperation(double n1, double n2);
+        static void Main(string[] args)
+        {
+            double a = 10;
+            double b = 12;
+
+            BinaryNumericOperation op = CalculatorService.ShowSum;
+            op += CalculatorService.ShowMax;
+
+            op.Invoke(a, b);
+        }
+    }
+}
