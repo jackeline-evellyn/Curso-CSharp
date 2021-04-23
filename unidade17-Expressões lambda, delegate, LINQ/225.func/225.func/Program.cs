dd859768e0be6dafc0c72cb0ff7b9a9a8188d27a
Jackeline Evellyn <jackelineeteixeira@gmail.com>
2021-04-23 16:27:54 -0300

    225.Referencia direta di metodo

---
+++ unidade17-Expressões lambda, delegate, LINQ/225.func/225.func/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using _225.func.Entities;
+using System.Linq;
+namespace _225.func
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Product> list = new List<Product>();
+
+            list.Add(new Product("Tv", 900.00));
+            list.Add(new Product("Mouse", 50.00));
+            list.Add(new Product("Tablet", 350.50));
+            list.Add(new Product("HD Case", 80.90));
+
+         
+            List<string> result = list.Select(NameUpper).ToList();
+            //função select retorna em Enumerable
+            //com .toList() é convertido para lista
+
+            foreach(string s in result)
+            {
+                Console.WriteLine(s);
+            }
+    
+        }
+        static string NameUpper(Product p)
+        {
+            return p.Name.ToUpper();
+        }
+
+    }
+}
