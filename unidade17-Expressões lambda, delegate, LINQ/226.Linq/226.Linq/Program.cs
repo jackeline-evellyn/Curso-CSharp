90292074b332d838fd4abf441295d86c87197fb2
Jackeline Evellyn <jackelineeteixeira@gmail.com>
2021-04-23 17:10:09 -0300

    226.Linq

---
+++ unidade17-Expressões lambda, delegate, LINQ/226.Linq/226.Linq/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Linq;
+
+namespace _226.Linq
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //3 passos
+
+            //1- Criar um data source
+            int[] numbers = new int[] { 2, 3, 4, 5 };
+
+            //2- Definir a query
+            var result = numbers.
+                Where(x => x % 2 == 0).
+                Select(x => x * 10);
+               //Antes do ponto realiza uma operação
+               //depois do ponto modifica o valor do resultado
+
+            //3- Executar a query
+            foreach (int x in result)
+            {
+                Console.WriteLine(x);
+            }
+        }
+    }
+}
