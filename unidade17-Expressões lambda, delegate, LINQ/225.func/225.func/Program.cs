10add59dd433ab2a0cfbe97cea026d92ec6dce0f
Jackeline Evellyn <jackelineeteixeira@gmail.com>
2021-04-23 16:31:41 -0300

    225. delegate

--- unidade17-Expressões lambda, delegate, LINQ/225.func/225.func/Program.cs
+++ unidade17-Expressões lambda, delegate, LINQ/225.func/225.func/Program.cs
@@ -15,8 +15,9 @@ namespace _225.func
             list.Add(new Product("Tablet", 350.50));
             list.Add(new Product("HD Case", 80.90));
 
-         
-            List<string> result = list.Select(NameUpper).ToList();
+            Func<Product, string> func = NameUpper;
+
+            List<string> result = list.Select(func).ToList();
             //função select retorna em Enumerable
             //com .toList() é convertido para lista
 
