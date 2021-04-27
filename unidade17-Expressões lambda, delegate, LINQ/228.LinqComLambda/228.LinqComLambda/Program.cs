e0f38a574029499d16c4a3defc03f8c6004934e5
Jackeline Evellyn <jackelineeteixeira@gmail.com>
2021-04-27 14:42:22 -0300

    linq

--- unidade17-Expressões lambda, delegate, LINQ/228.LinqComLambda/228.LinqComLambda/Program.cs
+++ unidade17-Expressões lambda, delegate, LINQ/228.LinqComLambda/228.LinqComLambda/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Globalization;
+using System.Linq;
 using _228.LinqComLambda.Entities;
 using System.Collections.Generic;
 
@@ -6,6 +8,14 @@ namespace _228.LinqComLambda
 {
     class Program
     {
+        static void Print<T>(string message, IEnumerable<T> collection)
+        {
+            Console.WriteLine(message);
+            foreach(T obj in collection)
+            {
+                Console.WriteLine(obj);
+            }
+        }
         static void Main(string[] args)
         {
             Category c1 = new Category() { Id = 1, Name = "Tools", Tier = 2 };
@@ -14,9 +24,25 @@ namespace _228.LinqComLambda
 
             List<Product> products = new List<Product>()
             {
-                new Product(){}
+                new Product(){Id= 1, Name= "Computer", Price= 1100.0, Category= c2 },
+                new Product(){Id= 2, Name= "Hammer", Price= 90.0, Category= c1 },
+                new Product(){Id= 3, Name= "TV", Price= 1700.0, Category= c3 },
+                new Product(){Id= 4, Name= "Notebook", Price= 1300.0, Category= c2 },
+                new Product(){Id= 5, Name= "Saw", Price= 80.0, Category= c1 },
+                new Product(){Id= 6, Name= "Tablet", Price= 700.00, Category= c2 },
+                new Product(){Id= 7, Name= "Camera", Price= 700.00, Category= c3 },
+                new Product(){Id= 8, Name= "Printer", Price= 350.0, Category= c3 },
+                new Product(){Id= 9, Name= "Macbook", Price= 1800.00, Category= c2 },
+                new Product(){Id= 10, Name= "Sound Bar", Price= 700.0, Category= c3 },
+                new Product(){Id= 11, Name= "Level", Price= 70.0, Category= c1 },
 
-            };
+
+                };
+
+            var r1 = products.Where(p => p.Category.Tier == 1 && p.Price < 900.0);
+            Console.WriteLine("Tier 1 AND Price < 900");
+                  
+           
 
         }
     }
