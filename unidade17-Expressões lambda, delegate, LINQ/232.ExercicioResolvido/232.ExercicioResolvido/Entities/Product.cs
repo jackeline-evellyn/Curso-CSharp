28fa4847d6362b8680b4dba3cfdd8752fde9a5cb
Jackeline Evellyn <jackelineeteixeira@gmail.com>
2021-04-28 17:12:02 -0300

    exercicio resolvido

---
+++ unidade17-Expressões lambda, delegate, LINQ/232.ExercicioResolvido/232.ExercicioResolvido/Entities/Product.cs
@@ -0,0 +1,18 @@
+﻿
+
+namespace _232.ExercicioResolvido.Entities
+{
+    class Product
+    {
+        public string Name { get; set; }
+        public double Price { get; set; }
+
+        public Product(string name, double price)
+        {
+            Name = name;
+            Price = price;
+        }
+
+
+    }
+}
---
+++ unidade17-Expressões lambda, delegate, LINQ/232.ExercicioResolvido/232.ExercicioResolvido/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using _232.ExercicioResolvido.Entities;
+using System.Collections.Generic;
+using System.IO;
+using System.Globalization;
+using System.Linq;
+
+namespace _232.ExercicioResolvido
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter full file path: ");
+            string path = Console.ReadLine();
+
+            List<Product> list = new List<Product>();
+
+            using (StreamReader sr = File.OpenText(path))
+            {
+                while (!sr.EndOfStream)
+                {
+                    string[] fields = sr.ReadLine().Split();
+                    string name = fields[0];
+                    double price = double.Parse(fields[1], CultureInfo.InvariantCulture);
+
+                    list.Add(new Product(name, price));
+                }
+            }
+            var 
+        }
+    }
+} 
