41ec25488fcca3a10f3def553f2fee6400afd614
Jackeline Evellyn <jackelineeteixeira@gmail.com>
2021-04-28 11:01:17 -0300

    Forma alternativa de realizar consultas: Linq com sintaxe similar ao SQL

---
+++ unidade17-Expressões lambda, delegate, LINQ/231.LinqSimilarSQL/231.LinqSimilarSQL/Entities/Category.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _231.LinqSimilarSQL.Entities
+{
+    class Category
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int Tier { get; set; } // Tier é a pontuação 
+    }
+}
--- unidade17-Expressões lambda, delegate, LINQ/231.LinqSimilarSQL/231.LinqSimilarSQL/Program.cs
+++ unidade17-Expressões lambda, delegate, LINQ/231.LinqSimilarSQL/231.LinqSimilarSQL/Program.cs
@@ -1,17 +1,17 @@
 ﻿using System;
 using System.Globalization;
 using System.Linq;
-using _228.LinqComLambda.Entities;
+using _231.LinqSimilarSQL.Entities;
 using System.Collections.Generic;
 
-namespace _228.LinqComLambda
+namespace _231.LinqSimilarSQL
 {
     class Program
     {
         static void Print<T>(string message, IEnumerable<T> collection)
         {
             Console.WriteLine(message);
-            foreach(T obj in collection)
+            foreach (T obj in collection)
             {
                 Console.WriteLine(obj);
             }
@@ -40,75 +40,89 @@ namespace _228.LinqComLambda
 
                 };
 
-            var r1 = products.Where(p => p.Category.Tier == 1 && p.Price < 900.0);
+            //var r1 = products.Where(p => p.Category.Tier == 1 && p.Price < 900.0);
+
+            var r1 =
+                from p in products
+                where p.Category.Tier == 1 && p.Price < 900
+                select p;
             Print("Tier 1 AND Price < 900", r1);
 
 
-            var r2 = products.Where(p => p.Category.Name == "Tools").Select(p => p.Name);
+            //var r2 = products.Where(p => p.Category.Name == "Tools").Select(p => p.Name);
+            var r2 =
+                from p in products
+                where p.Category.Name == "Tools"
+                select p.Name;
             Print("NAME OF PROUCTS FROM TOOLS", r2);
 
-            var r3 = products.Where(p => p.Name[0] == 'C').Select(p => new { p.Name, p.Price, CategoryName = p.Category.Name });
+            //var r3 = products.Where(p => p.Name[0] == 'C').Select(p => new { p.Name, p.Price, CategoryName = p.Category.Name });
+            var r3 =
+                from p in products
+                where p.Name[0] == 'C'
+                select new
+                {
+                    p.Name,
+                    p.Price,
+                    CategoryName = p.Category.Name
+                };
             Print("NAMES STARTED WITH 'C' AND ANONYMOUS OBJECT", r3);
 
-            var r4 = products.Where(p => p.Category.Tier == 1).OrderBy(p => p.Price).ThenBy(p=> p.Name);
+            //var r4 = products.Where(p => p.Category.Tier == 1).OrderBy(p => p.Price).ThenBy(p => p.Name);
+            var r4 =
+                from p in products
+                where p.Category.Tier == 1
+                orderby p.Name //ordem inversa de ordernação
+                orderby p.Price // será ordenado primeiro por preço e depois por nome
+                select p;
+
+
             Print("TIER 1 ORDER BY PRICE THEN BY NAME", r4);
 
-            var r5 = r4.Skip(2).Take(4); //Pula dois elementos e pega 4 
+            //var r5 = r4.Skip(2).Take(4); //Pula dois elementos e pega 4 
+            var r5 =
+                (from p in r4
+                 select p).Skip(2).Take(4); 
+            //faz a expressão normal similar ap sql 
+            //depois coloca-se o terminador necessário
+            
             Print("TIER 1 ORDER BY PRICE THEN BY NAME SKIP 2 TAKE 4", r5);
 
-            var r6 = products.First();
+            //var r6 = products.First();
+            var r6 = (from p in products select p).First(); // Nesse caso é um esforço denecessário
             Console.WriteLine("First test1: " + r6);
             //First caso nulo pode gerar uma exceção
 
-            var r7 = products.Where(p => p.Price > 3000.0).FirstOrDefault();
+            //var r7 = products.Where(p => p.Price > 3000.0).FirstOrDefault();
+            var r7 =
+                (from p in products
+                 where p.Price > 3000
+                 select p).FirstOrDefault();
+            //também é um esforço desnecessário
             Console.WriteLine("First or Default test2: " + r7);
             //First or default caso nao tenha nenhum resultado retorna default
             Console.WriteLine();
 
-            var r8 = products.Where(p => p.Id == 3).SingleOrDefault();
-            Console.WriteLine("Single or default: "+r8);
-            //Single or Default quando retorna mais que um valor retorna uma exceção
-
-            var r9 = products.Where(p => p.Id == 30).SingleOrDefault();
-            Console.WriteLine("Single or default: " + r9);
-
-            var r10 = products.Max(p => p.Price);
-            Console.WriteLine("Max price: " + r10);
-
-            var r11 = products.Min(p => p.Price);
-            Console.WriteLine("Min price: " + r11);
-
-            var r12 = products.Where(p => p.Category.Id == 1).Sum(p => p.Price);
-            Console.WriteLine("Category 1 Sum prices: " + r12);
-
-            var r13 = products.Where(p => p.Category.Id == 1).Average(p => p.Price);
-            Console.WriteLine("Category 1 Average prices: " + r13);
-
-            var r14 = products.Where(p => p.Category.Id == 5).Select(p => p.Price).DefaultIfEmpty(0.0).Average();
-            Console.WriteLine("Category 5 Average prices: " + r14);
-            //Dessa forma, caso seja nulo a media nao dara uma exceção
-
-            var r15 = products.Where(p => p.Category.Id == 1).Select(p => p.Price).Aggregate(0.0, (x,y) => x+y);
-            Console.WriteLine("Category 1 Agregate sum: "+ r15);
-
-            var r16 = products.Where(p => p.Category.Id == 5).Select(p => p.Price).Aggregate(0.0, (x, y) => x + y);
-            Console.WriteLine("Category 5 Agregate sum: " + r16);
-            Console.WriteLine();
 
             //Agrupar
-            var r17 = products.GroupBy(p => p.Category);
-            foreach(IGrouping<Category, Product> group in r17)
+            //var r17 = products.GroupBy(p => p.Category);
+
+            var r17 =
+                from p in products
+                group p by p.Category;
+            foreach (IGrouping<Category, Product> group in r17)
             {
                 Console.WriteLine("Category " + group.Key.Name + ":");
-                foreach(Product p in group)
+                foreach (Product p in group)
                 {
                     Console.WriteLine(p);
                 }
                 Console.WriteLine();
             }
 
+
         }
 
-        
+
     }
 }
