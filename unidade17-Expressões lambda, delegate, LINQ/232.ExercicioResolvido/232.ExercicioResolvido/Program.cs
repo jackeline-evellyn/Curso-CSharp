1ac2ceedb02e0ea4c5efbaf8d3557a71da167c8b
Jackeline Evellyn <jackelineeteixeira@gmail.com>
2021-04-29 16:31:37 -0300

    finalização capitulo lambda, delegate e LINQ

--- unidade17-Expressões lambda, delegate, LINQ/232.ExercicioResolvido/232.ExercicioResolvido/Program.cs
+++ unidade17-Expressões lambda, delegate, LINQ/232.ExercicioResolvido/232.ExercicioResolvido/Program.cs
@@ -11,7 +11,7 @@ namespace _232.ExercicioResolvido
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Enter full file path: ");
+            Console.Write("Enter full file path: ");
             string path = Console.ReadLine();
 
             List<Product> list = new List<Product>();
@@ -20,14 +20,23 @@ namespace _232.ExercicioResolvido
             {
                 while (!sr.EndOfStream)
                 {
-                    string[] fields = sr.ReadLine().Split();
+                    string[] fields = sr.ReadLine().Split(',');
                     string name = fields[0];
                     double price = double.Parse(fields[1], CultureInfo.InvariantCulture);
 
                     list.Add(new Product(name, price));
                 }
             }
-            var 
+            var avg = list.Select(p => p.Price).DefaultIfEmpty(0.0).Average();
+            Console.WriteLine("Average price= " +avg.ToString("F2",CultureInfo.InvariantCulture));
+
+
+            var names = list.Where(p => p.Price < avg).OrderByDescending(p => p.Name).Select(p=> p.Name);
+            foreach(string name in names)
+            {
+                Console.WriteLine(name);
+            }
+            
         }
     }
 } 
---
+++ unidade17-Expressões lambda, delegate, LINQ/233.ExercicioFixacao/233.ExercicioFixacao/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Globalization;
+using System.IO;
+using System.Collections.Generic;
+using _233.ExercicioFixacao.Entities;
+using System.Linq;
+
+namespace _233.ExercicioFixacao
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter full file path: ");
+            string path = Console.ReadLine();
+
+            List<Employee> employee = new List<Employee>();
+            
+            using(StreamReader sr = File.OpenText(path))
+            {
+                while (!sr.EndOfStream)
+                {
+                    string[] fields = sr.ReadLine().Split(',');
+                    string name = fields[0];
+                    string email = fields[1];
+                    double salary = double.Parse(fields[2], CultureInfo.InvariantCulture);
+
+                    employee.Add(new Employee(name, email, salary));
+                }
+            }
+
+            Console.Write("Enter salary: ");
+            double minSalary = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            var selectEmployee = employee.Where(p => p.Salary > minSalary).OrderBy(p => p.Email).Select(p => p.Email);
+            Console.WriteLine("Email of people whose salary is more than " +minSalary.ToString("F2", CultureInfo.InvariantCulture));
+
+            foreach( string email in selectEmployee)
+            {
+                Console.WriteLine(email);
+            }
+
+            double sum = employee.Where(p => p.Name[0] == 'M').Select(p => p.Salary).Sum();
+            Console.WriteLine("Sum of salary of people whose name starts with 'M': " + sum.ToString("F2", CultureInfo.InvariantCulture));
+            
+        }
+    }
+}
